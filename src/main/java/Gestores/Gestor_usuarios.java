b130dd462b75d2339dde7e07d73e85e493deb3f8
admin <andreadelr.lc@gmail.com>
2026-06-09 14:48:18 -0500

    historial login usando pilas

--- src/main/java/Gestores/Gestor_usuarios.java
+++ src/main/java/Gestores/Gestor_usuarios.java
@@ -11,13 +11,15 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Stack;
 
 /**
  *
  * @author admin
  */
 // Clase Gestor_usuarios
-public class Gestor_usuarios extends GestorBase <Administradores>{
+public class Gestor_usuarios extends GestorBase<Administradores> {
     /**
      * Idea de cambio:
      * Colas: Intento de login hasta 5 veces,si falla la cuenta se bloqueara
@@ -25,35 +27,38 @@ public class Gestor_usuarios extends GestorBase <Administradores>{
      */
 
     private static Gestor_usuarios instancia;
-    
+
+    // Pila para el historial de loggins
+    private Stack<Administradores> historialLogins = new Stack<>();
+
     public Gestor_usuarios() {
         super("TXT/Administradores.txt");
     }
-    
+
     public static Gestor_usuarios getInstanciaUsuario() {
-        if (instancia==null){
-            instancia= new Gestor_usuarios();
-        }  
-        return instancia; 
-    }         
-    
+        if (instancia == null) {
+            instancia = new Gestor_usuarios();
+        }
+        return instancia;
+    }
+
     @Override
     public void cargarDatos() {
         try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
             String linea;
             while ((linea = br.readLine()) != null) {
-                String[] datos = linea.split(",");              
+                String[] datos = linea.split(",");
                 if (datos.length == 6) {
                     int dni = Integer.parseInt(datos[0]);
                     String nombre = datos[1];
-                    String apellido= datos[2];
+                    String apellido = datos[2];
                     int telefono = Integer.parseInt(datos[3]);
                     String correo = datos[4];
                     String contraseña = datos[5];
-                    Administradores administradores= new Administradores(new Persona(dni,nombre,apellido,telefono,correo)
-                            ,contraseña);
-                    
-                    getElementos().put(correo,administradores);
+                    Administradores administradores = new Administradores(
+                            new Persona(dni, nombre, apellido, telefono, correo), contraseña);
+
+                    getElementos().put(correo, administradores);
                 }
             }
         } catch (IOException e) {
@@ -63,7 +68,7 @@ public class Gestor_usuarios extends GestorBase <Administradores>{
 
     @Override
     public boolean registrar(Administradores administrador) {
-        if (getElementos().containsKey(administrador.getCorreo())) {   //verifica si el map contiene la clave
+        if (getElementos().containsKey(administrador.getCorreo())) { // verifica si el map contiene la clave
             System.out.println("Ese correo ya esta registrado.");
             return false;
         }
@@ -72,20 +77,21 @@ public class Gestor_usuarios extends GestorBase <Administradores>{
 
         // Guardar en el HashMap (usando el correo como clave y la Persona como valor)
         getElementos().put(administrador.getCorreo(), administrador);
-        guardarCambios();       
+        guardarCambios();
         return true;
     }
-        @Override
+
+    @Override
     public void guardarCambios() {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(rutaArchivo))) {
             for (Administradores admin : getElementos().values()) {
-                String linea = String.format("%d,%s,%s,%d,%s,%s",          
-                    admin.getDni_persona(),           
-                    admin.getNombre(),
-                    admin.getApellido(),
-                    admin.getTelefono(),
-                    admin.getCorreo(),  
-                    admin.getContraseña());
+                String linea = String.format("%d,%s,%s,%d,%s,%s",
+                        admin.getDni_persona(),
+                        admin.getNombre(),
+                        admin.getApellido(),
+                        admin.getTelefono(),
+                        admin.getCorreo(),
+                        admin.getContraseña());
                 bw.write(linea);
                 bw.newLine();
             }
@@ -94,49 +100,76 @@ public class Gestor_usuarios extends GestorBase <Administradores>{
         }
     }
 
-
     public boolean login(String correo, String contraseña) {
+        // Verificar si existe algun correo en el sistema
         if (getElementos().isEmpty()) {
             System.out.println("No hay usuarios registrados en el sistema.");
             return false;
         }
-        if (!getElementos().containsKey(correo)) {   
+        // Verificar si el correo existe en el sistema( en el hashmap)
+        if (!getElementos().containsKey(correo)) {
             return false; // correo (usuario) no encontrado
-            
+
         }
         Administradores valor = getElementos().get(correo); // devolvera el valor asociado a la clave "correo"
-        
+        historialLogins.push(valor);// Agregar al historial de logins
+
         if (valor != null && valor.getContraseña().equals(contraseña)) {
-            System.out.println("Bienvenid@ "+ valor.getNombre());
-            return true;  // login correcto
+            System.out.println("Bienvenid@ " + valor.getNombre());
+            guardarHistorial(); // Guardar el historial de logins exitoso en el archivo
+            return true; // login correcto
         } else {
             return false; // contraseña incorrecta
         }
     }
 
+    public void guardarHistorial() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("TXT/HistorialLogins.txt"))) {
+            for (Administradores admin : historialLogins) {
+                String linea = String.format("%d,%s,%s,%d,%s,%s,%s",
+                        admin.getDni_persona(),
+                        admin.getNombre(),
+                        admin.getApellido(),
+                        admin.getTelefono(),
+                        admin.getCorreo(),
+                        admin.getContraseña(),
+                    );
+                bw.write(linea);
+                bw.newLine();
+            }
+        } catch (IOException ex) {
+            System.out.println("Error al guardar cambios en el archivo de admiistradores");
+        }
+    }
+
     @Override
     public boolean existe(String identificador) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from
+                                                                       // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public void mostrar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from
+                                                                       // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public void modificar(String datoModificar, int opcion) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from
+                                                                       // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public void buscar(String identificador) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from
+                                                                       // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public boolean eliminar(String identificador) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from
+                                                                       // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
